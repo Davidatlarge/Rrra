d799d1d4f64a92182c4a7ec5d671b1f2cc3df21f
Kaiser <david.kaiser@hereon.de>
2023-05-11 19:16:25 +0200

    minor

--- process_sample.R
+++ process_sample.R
@@ -25,55 +25,22 @@ CPM220 <- Rn$count.summary$CPM220
 CPM219 <- Rn$count.summary$CPM219
 detector <- Rn$detector
 
-## process sample - NEEDS ONLY DATA FROM SINGLE COUNT
-midpoint <- counting_midpoint(start.time = start.time, Runtime = Runtime) # hard to compare with excel cos of different formats
-
-# Decay Factor
+# Decay Factor - is this used again?
 decay.factor223 <- decay_factor(223, midpoint = midpoint, sampling.time = sampling.time) # not the same as in excel - rounding?!
 decay.factor224 <- decay_factor(224, midpoint = midpoint, sampling.time = sampling.time) # not the same as in excel - rounding?!
 
-# ERR Total CPM - NEEDS ONLY DATA FROM SINGLE COUNT
-total.counts <- Runtime * CPMTot
-err.total.cpm <- sqrt(total.counts) / Runtime
+#
+# MISSING PARTS ARE HANDLED BY process_rn() AND CAN BE TAKEN BACK FROM THERE EASILY
+#
 
-# Background - NEEDS ONLY DATA FROM SINGLE COUNT
-cc.220 <- ((CPMTot-CPM220-CPM219)^2*0.01) / ((1-((CPMTot-CPM220-CPM219)*0.01)))
-cc.219 <- ((CPMTot-CPM220-cc.220-CPM219)^2*0.000093) / ((1-(CPMTot-CPM220-cc.220-CPM219)*0.000093))
-
-# Err 220 CPM - NEEDS ONLY DATA FROM SINGLE COUNT
-counts.220 <- Runtime * CPM220
-err.220.cpm <- sqrt(counts.220) / Runtime
-err.220.2by <- 2/sqrt(counts.220)
-err.220 <- sqrt(counts.220-(cc.220*Runtime)) / (counts.220-(cc.220*Runtime))
-
-# Err 219 CPM - NEEDS ONLY DATA FROM SINGLE COUNT
-counts.219 <- Runtime * CPM219
-err.219.cpm <- sqrt(counts.219) / Runtime
-err.219.2by <- 2/sqrt(counts.219)
-err.219 <- sqrt((CPM219*total.counts)-(cc.219*total.counts))/((CPM219*total.counts)-(cc.219*total.counts))
-
-# Background (cont.) - NEEDS ONLY DATA FROM SINGLE COUNT
-x <- CPMTot-CPM220-CPM219
-err.x <- sqrt((err.total.cpm^2)+(err.220.cpm^2)+(err.219.cpm^2))
-err.cc.220 <- err.x*(((2*0.01*x)-(0.01*x)^2)/(1-0.01*x)^2)
-err.corr.220 <- sqrt((err.220.cpm^2)+(err.cc.220^2)) #  this belongs in the Corrections group but is needed before
-err.y <- sqrt((err.219.cpm^2)+(err.corr.220^2)+(err.total.cpm^2))
-err.cc.219 <- err.y*(((2*0.000093*x-(0.01*x))^2)/(1-(0.000093*x)^2))
-
-# Corrections - NEEDS ONLY DATA FROM SINGLE COUNT
-corr.220 <- CPM220-cc.220
-err.corr.220
-corr.219 <- CPM219-cc.219
-err.corr.219 <- sqrt((err.219.cpm^2)+(err.cc.219^2))
-
-# efficiency
+# efficiency - THIS IS COVERED BY final_rn()
 detector.eff.223 <- eff$mean[eff$detector==detector & eff$isotope==223]
 detector.eff.223.sd <- eff$sd[eff$detector==detector & eff$isotope==223]
 detector.eff.224 <- eff$mean[eff$detector==detector & eff$isotope==224]
 detector.eff.224.sd <- eff$sd[eff$detector==detector & eff$isotope==224]
 effic <- (((detector.eff.223*2)*(CPM219-cc.219))^2*0.01) / (1+((detector.eff.223*2)*(CPM219-cc.219))*0.01) # very minor difference from excel, when copying the efficiency value
 
-# Final
+# Final - THIS IS COVERED BY final_rn()
 detector.blank.220 <- blk$mean[blk$detector==detector & blk$isotope==224]
 
 final.220 <- corr.220-effic-detector.blank.220
