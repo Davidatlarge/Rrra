fb5311f41744b0e783970e460fb33b6dc7a06935
Kaiser <david.kaiser@hereon.de>
2023-05-11 15:44:52 +0200

    make folder for functions

--- functions/read_rn.R
+++ functions/read_rn.R
@@ -6,19 +6,28 @@ read_rn <- function(file) {
   
   # extract the original file name
   original.filename <- gsub(".*\\\\(.*)", "\\1", filelines[1])
+  filename <- sub(".*[\\\\|/]", "", file)
+  
+  # extract the measurement type
+  source("functions/identify_type.R")
+  type <- identify_type(file)
+
+  # extract detector from file name
+  detector <- sub(".*(orange|blue|grey|green).*", "\\1", file)
+  if(!(detector %in% c("orange","blue","grey","green"))) {warning("detector not found")}
   
   # extract start time
   start.time <- filelines[grep("Start Time", filelines)]
   start.time <- sub("Start Time ", "", start.time)
   start.time <- gsub(" +", "T", start.time)
-  start.time <- as.POSIXct(start.time, format = "%d/%m/%YT%H:%M:%S")
+  start.time <- as.POSIXct(start.time, format = "%m/%d/%YT%H:%M:%S")
   
   # extract stop time
   if(any(grepl("Stopped", filelines))) {
     end.time <- filelines[grep("Stopped", filelines)]
     end.time <- sub(".*Stopped ", "", end.time)
     end.time <- gsub(" +", "T", end.time)
-    end.time <- as.POSIXct(end.time, format = "%d/%m/%YT%H:%M:%S")
+    end.time <- as.POSIXct(end.time, format = "%m/%d/%YT%H:%M:%S")
   } else {
     end.time <- NA
     warning(paste0("Stop Time not available in ", file,". Returning NA"))
@@ -27,8 +36,8 @@ read_rn <- function(file) {
   # extract Count Summary
   if(any(grepl("Count Summary", filelines))) {
     count.summary <- filelines[grep("Count Summary", filelines)+c(2,3)]
-    count.summary <- t(data.frame(row.names = unlist(strsplit(count.summary[1], split = " +")),
-                                  value = as.numeric(unlist(strsplit(count.summary[2], split = " +")))))
+    count.summary <- as.data.frame(t(data.frame(row.names = unlist(strsplit(count.summary[1], split = " +")),
+                                  value = as.numeric(unlist(strsplit(count.summary[2], split = " +"))))))
   } else {
     count.summary <- NA
     warning(paste0("Count Summary not available in ", file,". Returning NA"))
@@ -56,8 +65,10 @@ read_rn <- function(file) {
   }
     
     # return result
-    return(list(file = file,
+    return(list(filename = filename,
                 original.filename = original.filename,
+                type = type,
+                detector = detector,
                 start.time = start.time,
                 end.time = end.time,
                 count.summary = count.summary,
@@ -65,4 +76,4 @@ read_rn <- function(file) {
   
 }
 
-#read_rn("data/AL557/Count1/050621_1grey_St10_blank.txt")
+#read_rn(file = "data/AL557/Count1/050621_1grey_St10_blank.txt")
