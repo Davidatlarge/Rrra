83c64459e895d9f963bee7152ad0990b87a3ccd9
Kaiser <david.kaiser@hereon.de>
2023-05-11 15:45:17 +0200

    make decay_factor()

---
+++ functions/decay_factor.R
@@ -0,0 +1,9 @@
+decay_factor <- function(isotope, # 223; 224
+                         midpoint, # calculated with calculate_midpoint()
+                         sampling.time # as POCIXct with date and time
+) {
+  hl <- data.frame(iso = c(223, 224),
+                   hl = c(11.434, 3.66))
+  return(exp(-log(2) / hl$hl[hl$iso==isotope] * as.numeric(midpoint-sampling.time) ))
+}
+#decay_factor(11.434, )
