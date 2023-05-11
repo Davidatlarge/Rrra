e242e027ca82ed493ae07bed40d8d176237e01de
Kaiser <david.kaiser@hereon.de>
2023-05-11 16:53:02 +0200

    finish count 1 sheet, excluding one link to count 2

--- process_sample.R
+++ process_sample.R
@@ -2,7 +2,7 @@
 sapply(list.files("functions/", full.names = T), source)
 
 # manual/constants
-Estimate.227Ac <- 0.05
+estimate.227Ac <- 0.05
 
 # metadata
 filtration_volume_L <- 200.5
@@ -80,14 +80,24 @@ err.dpm.220 <- sqrt( (err.final.220/detector.eff.224)^2 + (final.220*detector.ef
 dpm.220per100L <- dpm.220/filtration_volume_L*100
 err.dpm.220per100L <- sqrt( (err.dpm.220/filtration_volume_L)^2 + (dpm.220*filtration_volume_L*0.03/filtration_volume_L^2)^2 ) * 100
 
-
 # Derived from Count 2
+fiber.228Th <- 10.33
+
 # Decay Corrected. - these are the final values but depend on count 2
-# Th estimate 
-# Final 224 Values - same as Decay corrected
+decay.corr.dpm.220.100L <- (dpm.220per100L-fiber.228Th)/decay.factor224
+err.decay.corr.dpm.220.100L <- (err.dpm.220per100L/dpm.220per100L)*decay.corr.dpm.220.100L
+
+# Th estimate
+rel.err.228Th <- err.decay.corr.dpm.220.100L/decay.corr.dpm.220.100L
+est.228Th.dpm.100L <- sqrt(CPM220*Runtime-cc.220*Runtime) / (CPM220*Runtime-cc.220*Runtime)
+
+# Final 224 Values
+Ra224.dpm.100L <- decay.corr.dpm.220.100L
+err.Ra224.dpm.100L <- err.decay.corr.dpm.220.100L
+err.Ra224.percent <- err.decay.corr.dpm.220.100L/decay.corr.dpm.220.100L*100
 
 # Calculating 219 from 220
-qm <- sqrt(CPM220*Runtime)/(CPM220*Runtime)
+qm <- sqrt(CPM220*Runtime)/(CPM220*Runtime) # qm means questionmark, thats in the excel sheet
 from.220 <- corr.220*0.0255
 final.219 <- corr.219-from.220
 err.final.219 <- sqrt(err.corr.219^2 + (0.0255*err.corr.220)^2)
@@ -95,3 +105,11 @@ dpm.219 <- final.219/detector.eff.223
 err.dpm.219 <- sqrt( (err.final.219/detector.eff.223)^2 + (final.219*detector.eff.223.sd/detector.eff.223^2)^2 ) # the detector.eff.223.sd term is ambiguous in the excel sheet but makes more sense than detector.eff.223
 dpm.219per100L <- dpm.219/filtration_volume_L*100
 err.dpm.219per100L <- sqrt( (err.dpm.219/filtration_volume_L)^2 + (dpm.219*filtration_volume_L*0.03/filtration_volume_L^2)^2 ) * 100
+
+decay.corr.dpm.219.100L <- (dpm.219per100L-estimate.227Ac)/decay.factor223
+err.decay.corr.dpm.219.100L <- (err.dpm.219per100L/dpm.219per100L)*decay.corr.dpm.219.100L
+
+# Final 223 Values
+Ra223.dpm.100L <- decay.corr.dpm.219.100L
+err.Ra223.dpm.100L <- err.decay.corr.dpm.219.100L
+err.Ra223.percent <- err.decay.corr.dpm.219.100L/decay.corr.dpm.219.100L*100
