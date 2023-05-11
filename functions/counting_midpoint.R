bc7e431af53dad01afba532151054d3e2d4f4be6
Kaiser <david.kaiser@hereon.de>
2023-05-11 15:46:00 +0200

    make counting_midpoint()

---
+++ functions/counting_midpoint.R
@@ -0,0 +1,7 @@
+counting_midpoint <- function(start.time, # As POSIXct
+                               Runtime # in minutes
+                               ) {
+  # adding the run time minutes to the start time
+  return(start.time + (Runtime*60)) # R adds numbers as seconds, so x60 for minutes
+}
+#calculate_midpoint(as.POSIXct("2021/06/05 10:42:00"), 40.4)
