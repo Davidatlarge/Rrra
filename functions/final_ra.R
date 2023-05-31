8071ac0c788f1c0ae63a0a857570c8aa70f5b870
Kaiser <david.kaiser@hereon.de>
2023-05-31 11:58:53 +0200

    include calculations of final 219 value in final_ra(); i.e. from excel columns "calculating 219 from 220"

--- functions/final_ra.R
+++ functions/final_ra.R
@@ -1,5 +1,5 @@
 # calculates those parameter of the count 1 sheet that need input of efficiency, blanks and filtration volume
-# other parameters calculates in count 1 sheet are supplied as a dataframe (pro), the output of process_ra()
+# other parameters calculated in count 1 sheet are supplied as a dataframe (pro), the output of process_ra()
 final_ra <- function(eff,
                      blk,
                      pro,
@@ -13,7 +13,7 @@ final_ra <- function(eff,
   
   effic <- (((detector.eff.223*2)*(pro$CPM219-pro$cc.219))^2*0.01) / (1+((detector.eff.223*2)*(pro$CPM219-pro$cc.219))*0.01) # very minor difference from excel, when copying the efficiency value
   
-  # Final
+  # Final 220 values
   detector.blank.220 <- blk$mean[blk$detector==pro$detector & blk$isotope==224]
   
   final.220 <- pro$corr.220-effic-detector.blank.220
@@ -23,6 +23,16 @@ final_ra <- function(eff,
   dpm.220per100L <- dpm.220/filtration_volume_L*100
   err.dpm.220per100L <- sqrt( (err.dpm.220/filtration_volume_L)^2 + (dpm.220*filtration_volume_L*0.03/filtration_volume_L^2)^2 ) * 100
   
+  # Final 219 values
+  qm <- sqrt(pro$CPM220*pro$Runtime) / (pro$CPM220*pro$Runtime) # qm means questionmark, thats in the excel sheet - NOT USED LATER?!
+  final.219 <- pro$corr.219 - pro$corr.220*0.0255 # c1$corr.220*0.0255 is "219 from 220" in excel
+  err.final.219 <- sqrt(pro$err.corr.219^2 + (0.0255*pro$err.corr.220)^2)
+  dpm.219 <- final.219/detector.eff.223
+  err.dpm.219 <- sqrt( (err.final.219/detector.eff.223)^2 + (final.219*detector.eff.223.sd/detector.eff.223^2)^2 ) # the detector.eff.223.sd term is ambiguous in the excel sheet but makes more sense than detector.eff.223
+  dpm.219per100L <- dpm.219/filtration_volume_L*100
+  err.dpm.219per100L <- sqrt( (err.dpm.219/filtration_volume_L)^2 + (dpm.219*filtration_volume_L*0.03/filtration_volume_L^2)^2 ) * 100
+  
+  # output
   return(data.frame(file = sub(".*[\\\\|/]", "", pro$file),
                     effic = effic,
                     final.220 = final.220,
@@ -30,14 +40,22 @@ final_ra <- function(eff,
                     dpm.220 = dpm.220,
                     err.dpm.220 = err.dpm.220,
                     dpm.220per100L = dpm.220per100L,
-                    err.dpm.220per100L = err.dpm.220per100L) )
+                    err.dpm.220per100L = err.dpm.220per100L,
+                    qm = qm, 
+                    final.219 = final.219,
+                    err.final.219 = err.final.219,
+                    dpm.219 = dpm.219,
+                    err.dpm.219 = err.dpm.219,
+                    dpm.219per100L = dpm.219per100L,
+                    err.dpm.219per100L = err.dpm.219per100L) )
 }
 
-source("functions/read_ra.R")
-source("functions/summarise_efficiency.R")
-source("functions/summarise_blank.R")
-source("functions/process_ra.R")
-final_ra(eff = summarise_efficiency(list.files("data/AL557/Standards/", full.names = TRUE)),
-         blk = summarise_blank(list.files("data/AL557/Count1/", full.names = TRUE)),
-         pro = process_ra(read_ra(file = "data/AL557/Count1/050621_1grey_St3.txt")),
-         filtration_volume_L <- 200)
+# source("functions/read_ra.R")
+# source("functions/summarise_efficiency.R")
+# source("functions/summarise_blank.R")
+# source("functions/process_ra.R")
+# t( final_ra(eff = summarise_efficiency(list.files("data/AL557/Standards/", full.names = TRUE)),
+#             blk = summarise_blank(list.files("data/AL557/Count1/", full.names = TRUE)),
+#             pro = process_ra(read_ra(file = "data/test_case1/050621_1grey_St3.txt")),
+#             filtration_volume_L <- 200)
+# )
