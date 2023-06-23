af3810f201046798d55b91f296748ae6c54e5f47
Kaiser <david.kaiser@hereon.de>
2023-06-23 18:14:01 +0200

    see last

--- functions/summarise_efficiency.R
+++ functions/summarise_efficiency.R
@@ -8,6 +8,8 @@
 summarise_efficiency <- function(files,
                                  standard.id = "standard|std", # string to identify standards in the file name
                                  blank.id = "blank",
+                                 dpm.223.std = 9.94, # Personal Comm Walter Geibert (AWI)
+                                 dpm.224.std = 12.1, # Personal Comm Walter Geibert (AWI)
                                  summarise = TRUE # should the results of all files be summarised, if FALSE returns a table of individual efficiencies
 ) {
   # find relevant files
@@ -18,12 +20,35 @@ summarise_efficiency <- function(files,
   # calculate efficiencies from standard measurements
   eff <- data.frame()
   for(std in standards) {
-    eff <- rbind(eff,
-                 calculate_efficiency(read_ra(std)) )
+    Ra <- read_ra(std)
+    type <- identify_type(std, standard.id = standard.id, blank.id = blank.id)
+    CPM219 <- Ra$count.summary$CPM219
+    CPM220 <- Ra$count.summary$CPM220
+    CPMTot <- Ra$count.summary$CPMTot
+    
+    cc.219 <- ((CPMTot-CPM220-CPM219)^2*0.000093) / (1-((CPMTot-CPM220-CPM219)*0.000093))
+    cc.220 <- ((CPMTot-CPM220-CPM219)^2*0.01) / (1-((CPMTot-CPM220-CPM219)*0.01))
+    
+    ## for isotope 223
+    fr.220 <- (CPM220-cc.220)*0.0255 #  this is 219 fr.220 in excel
+    corr.219 <- CPM219-cc.219-fr.220
+    eff.219 <- corr.219/dpm.223.std
+    
+    ## for isotope 224
+    fr.219 <- ((1.6*(CPM219-cc.219))^2*0.01) / ((1-(1.6*(CPM219-cc.219))*0.01))
+    corr.220 <- CPM220-cc.220-fr.219
+    eff.220 <- corr.220/dpm.224.std
+    
+    eff <- rbind(eff, data.frame(filename = Ra$filename,
+                                 type = type,
+                                 isotope = as.numeric(sub("(223|224).*", "\\1", type)),
+                                 detector = Ra$detector,
+                                 eff.220 = eff.220,
+                                 eff.219 = eff.219))
   }
   
+  # summarise results
   if(summarise) {
-    # summarise results
     eff <- reshape(eff, direction = "long", # turn to long table for aggregating
                    v.names = "value", 
                    varying = names(eff)[5:6], 
