ae6ed25067746d08afef4e92cb0db569251fe3b1
Kaiser <david.kaiser@hereon.de>
2023-06-23 12:17:38 +0200

    bugfix

--- functions/estimate_228Th.R
+++ functions/estimate_228Th.R
@@ -1,6 +1,6 @@
 # Th estimate from measured values of one count, typically the first count
 estimate_228Th <- function(Ra # the output of process_ra()
                            ) {
-  return(sqrt(Ra$CPM220*Ra$Runtime - Ra$cc.220*Ra$Runtime) / (Ra$CPM220*c1$Runtime - Ra$cc.220*Ra$Runtime))
+  return(sqrt(Ra$CPM220*Ra$Runtime - Ra$cc.220*Ra$Runtime) / (Ra$CPM220*Ra$Runtime - Ra$cc.220*Ra$Runtime))
 }
 #estimate_228Th(process_ra(read_ra("data/test_case1/050621_1grey_St3.txt")))
