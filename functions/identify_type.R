338239e0dda6f3fc838a2849ce3bf3b3a7d11599
Kaiser <david.kaiser@hereon.de>
2023-06-23 16:22:04 +0200

    handle missing arguments

--- functions/identify_type.R
+++ functions/identify_type.R
@@ -5,26 +5,33 @@
 # multiple options can be passed using the | character, e.g. "standard|std"
 # the search for th IDs in the file name is case-insensitive, so "STD" and "Std" will both work
 identify_type <- function(string, # generally a file name
-                          blank.id = "blank", 
-                          standard.id = "standard|std" 
+                          standard.id,
+                          blank.id
 ) {
+  # handle missing arguments
+  if(missing(standard.id) & missing(blank.id)) stop("at least one of 'standard.id' or 'blank.id' arguments must be supplied")
+  
   # remove leading path from file names
   string <- sub(".*[\\\\|/]", "", string)
+  
   # identify type of sample in file name
-  if(grepl(standard.id, string, ignore.case = TRUE) &
-     grepl(blank.id, string, ignore.case = TRUE)) {
+  is_std <- if(!missing(standard.id)) grepl(standard.id, string, ignore.case = TRUE) else FALSE
+  is_blk <- if(!missing(blank.id)) grepl(blank.id, string, ignore.case = TRUE) else FALSE
+  
+  if(is_std & is_blk) {
     type <- "standard_blank"
-  } else if(grepl(standard.id, string, ignore.case = TRUE)) {
+  } else if(is_std) {
     type <- "standard"
-  } else if(grepl(blank.id, string, ignore.case = TRUE)) {
+  } else if(is_blk) {
     type <- "blank"
   } else {
     type <- "sample"
   }
+  
   # add the isotope number to standards, if any
   if(grepl("standard", type) &
      grepl("223|224", string)
-     ) { 
+  ) { 
     type <- paste0(sub(".*(223|224).*", "\\1", string), "_", type) 
   }
   
@@ -32,6 +39,8 @@ identify_type <- function(string, # generally a file name
 }
 
 # identify_type(string = "050621_1grey_St3.txt")
-# identify_type(string = "050621_1grey_St10_blank.txt")
-# identify_type(string = "050621_1orange_228Rastandard.txt")
-# identify_type(string = "060621_blue_223STD_blank.txt")
+# identify_type(string = "050621_1grey_St3.txt", blank.id = "blank", standard.id = "standard|std")
+# identify_type(string = "050621_1grey_St10_blank.txt", blank.id = "blank")
+# identify_type(string = "050621_1orange_228Rastandard.txt", blank.id = "blank", standard.id = "standard|std")
+# identify_type(string = "060621_blue_223STD_blank.txt", blank.id = "blank", standard.id = "standard|std")
+
