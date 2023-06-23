f32c4705de1ca561cc1df2f6f55ac0a6820f93b5
Kaiser <david.kaiser@hereon.de>
2023-06-23 12:02:55 +0200

    warning

--- functions/counting_midpoint.R
+++ functions/counting_midpoint.R
@@ -1,8 +1,11 @@
 counting_midpoint <- function(start.time, # As POSIXct
                               Runtime # in minutes
 ) {
+  if(!inherits(start.time, "POSIXct")) {stop("start.time must be in POSIXct format")}
+  
   # adding the run time minutes to the start time
   return(start.time + (Runtime*60)) # R adds numbers as seconds, so x60 for minutes
 }
 
-#calculate_midpoint(as.POSIXct("2021/06/05 10:42:00"), 40.4)
+#counting_midpoint(as.Date("2021/06/05 10:42:00"), 40.4)
+#counting_midpoint(as.POSIXct("2021/06/05 10:42:00"), 40.4)
